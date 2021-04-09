defb4b81ea5b8b3f75d75471b45bd5fb965e1203
deniskadrich <dev4long@live.com>
2021-04-09 15:37:02 -0400

    partial update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
+let submitForm = document.querySelector('form#create-task-form');
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
-console.log('create repo')
+  
+  });
+
+
+submitForm.addEventListener('click', submitClick)
+
+
+
+
+function submitClick(event){
+  event.preventDefault()
+  let input = event.target.querySelector('input#new-task-description').value
+  let li = document.createElement("li")
+  li.innerText = input
+  comments.append(li)
+  event.target.reset()
+}
+
+
